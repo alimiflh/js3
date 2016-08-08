f1b005a596d5588b3b4fac516cacec3a159dd5da
alimiflh <alimiflh@gmail.com>
2016-08-08 22:16:53 +0300

    js3

---
+++ js/new_file.js
@@ -0,0 +1,20 @@
+var arrayali=[2,4,6,8,10,12,14,16,18,20];
+
+console.log(arrayali.length);
+
+console.log(arrayali[0]);
+console.log(arrayali[2]);
+console.log(arrayali[5]);
+console.log(arrayali.indexOf(10));
+
+arrayali.splice(8,2);
+console.log(arrayali);
+
+var arrayali2=[2,5,"שתיים","חמש"];
+console.log(arrayali2);
+
+arrayali2.shift();
+console.log(arrayali2);
+
+var classweb=["mazen","ali","wafi"];
+var mazen=
